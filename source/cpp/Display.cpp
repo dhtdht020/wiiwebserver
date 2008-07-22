cf85039d0e8ceda9d6f2b2279a8f51c6a65c2658
henke37_2 <henke37_2@8cb5f321-4266-4168-a506-8f56a8e9aecc>
2008-07-22 21:48:25 +0000

    moving to wiisprite

--- source/cpp/Display.cpp
+++ source/cpp/Display.cpp
@@ -7,23 +7,22 @@ extern "C" {
 
 #include "globals.h"
 
-static void *xfb;
-static GXRModeObj *rmode;
+#include <wiisprite.h>
+
+using namespace wsp;
 
 void *Display(void*) {
-	VIDEO_Init();
+
 	PAD_Init();
 	WPAD_Init();
-	
-	rmode = VIDEO_GetPreferredMode(NULL);
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	//console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-	VIDEO_Configure(rmode);
-	VIDEO_SetNextFramebuffer(xfb);
-	VIDEO_SetBlack(TRUE);
-	VIDEO_Flush();
 
+	GameWindow gwd;
+
+	gwd.InitVideo();
+	
 	//TODO: add input and display proccessing loop
+
+	gwd.StopVideo();
 
 	return (void*)0;
 }
