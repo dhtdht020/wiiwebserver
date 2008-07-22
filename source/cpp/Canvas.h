cdec3b5c6b895c78f0f38494aa3847d0e78ffb90
henke37_2 <henke37_2@8cb5f321-4266-4168-a506-8f56a8e9aecc>
2008-07-22 22:57:12 +0000

    const correctnes and private flodfill work

--- source/cpp/Canvas.h
+++ source/cpp/Canvas.h
@@ -3,6 +3,9 @@
 #include <wiisprite.h>
 using namespace wsp;
 
+#include <set>
+using std::set;
+
 class Canvas : public Image {
 public:
 
@@ -10,6 +13,8 @@ public:
 	public:
 		u8 r,g,b,a;
 		void operator!(void);
+		bool operator!=(const pixel&);
+		bool operator==(const pixel&);
 	};
 
 	class position {
@@ -17,6 +22,7 @@ public:
 		position(unsigned int x,unsigned int y);
 		unsigned int x,y;
 		
+		bool operator <(const position &p2) const;
 	};
 
 	Canvas(void);
@@ -25,12 +31,18 @@ public:
 	Canvas::pixel &operator() (unsigned int x, unsigned int y);
 
 	//extra fast line drawing operators
-	void hline(unsigned int x1,unsigned int x2,unsigned int y,const pixel &p);
-	void vline(unsigned int x,unsigned int y1,unsigned int y2,const pixel &p);
-	void filledRect(unsigned int x1, unsigned int x2, unsigned int y1, unsigned int y2, const pixel &p);
-	void floodFill(unsigned int x, unsigned int y, const pixel &p);
+	void hline(const unsigned int x1,const unsigned int x2,const unsigned int y,const pixel &p);
+	void vline(const unsigned int x,const unsigned int y1, const unsigned int y2,const pixel &p);
+	//non constnes is not accidental
+	void simple45line(unsigned int x,unsigned int y, const unsigned int lenght
+	void filledRect(const unsigned int x1, const unsigned int x2, const unsigned int y1, const unsigned int y2, const pixel &p);
+	void floodFill(const unsigned int x, const unsigned int y, const pixel &p);
 
 	void operator!(void);
 
 	void flush(void);
+
+private:
+	//used by flood fill
+	void tryToAdd(set<position> &open,bool *closed,const pixel &p,signed int xOffset,signed int yOffset,const position &pos);
 };
